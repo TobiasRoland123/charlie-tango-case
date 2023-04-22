404199bbd73d578475d990dbccc6a9cfa38ab6d0
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2023-04-22 14:46:36 +0200

    Made Anchor.jsx ++ added 1st step on site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Anchor from "@/components/Header/Anchor";
 import styles from "./Home.module.css";
 
 export default function Home() {
@@ -8,38 +9,58 @@ export default function Home() {
         <title>Find buyer | EDC</title>
       </Head>
       <div className="wrapper">
-        <h1 className={styles.headline}>Hello MMD</h1>
-        <div className={styles.content}>
-          <p>
-            To get started, edit <code>pages/index.js</code> and save to reload.
-          </p>
-        </div>
-        <div className={styles.content}>
-          <h2>Basic form example</h2>
-          <p>
-            This is simple example of how you could submit a form to another
-            page in Next.js, without using a custom <code>submit</code> function
-            (e.g. without JavaScript). It is unstyled and unfinished. You can
-            use this as base, or implement your own solution.
-          </p>
-          <p>
-            Make sure to read the guide on{" "}
-            <a
-              href="https://nextjs.org/docs/guides/building-forms"
-              target="_blank"
-            >
-              building forms in Next.js
-            </a>
-          </p>
-          <form action="/buyers" method="GET" className={styles.form}>
-            <label>
-              <span className={styles.label}>Zip Code</span>
-              <input name="zipCode" required />
-            </label>
-            <button className={styles.button}>Submit</button>
-          </form>
-        </div>
+        <h1 className={styles.headline}>Welcome to EDC Seller Helper</h1>
+        <h3>
+          With EDCs patentet Seller Helper<>&trade;</>, Well help to connect you
+          with potential buyers in three simple steps!
+        </h3>
+      </div>
+      <div className="wrapper front">
+        <Anchor href="/estatedetails">
+          <button className={styles.button}>Find buyers for your estate</button>
+        </Anchor>
+      </div>
+      <div className="wrapper">
+        <h4>
+          <Anchor className="link--dashboard" href="#">
+            EDC Broker? Press here to see dashboard.
+          </Anchor>
+        </h4>
       </div>
     </>
   );
 }
+
+// <div className="wrapper">
+//   <h1 className={styles.headline}>Hello MMD</h1>
+//   <div className={styles.content}>
+//     <p>
+//       To get started, edit <code>pages/index.js</code> and save to reload.
+//     </p>
+//   </div>
+//   <div className={styles.content}>
+//     <h2>Basic form example</h2>
+//     <p>
+//       This is simple example of how you could submit a form to another
+//       page in Next.js, without using a custom <code>submit</code> function
+//       (e.g. without JavaScript). It is unstyled and unfinished. You can
+//       use this as base, or implement your own solution.
+//     </p>
+//     <p>
+//       Make sure to read the guide on{" "}
+//       <a
+//         href="https://nextjs.org/docs/guides/building-forms"
+//         target="_blank"
+//       >
+//         building forms in Next.js
+//       </a>
+//     </p>
+//     <form action="/buyers" method="GET" className={styles.form}>
+//       <label>
+//         <span className={styles.label}>Zip Code</span>
+//         <input name="zipCode" required />
+//       </label>
+//       <button className={styles.button}>Submit</button>
+//     </form>
+//   </div>
+// </div>;
