19c87cfa3147054234c24b254ae6593011a44d0d
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-04-27 16:05:19 +0200

    Data on dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,8 +3,22 @@
 import Head from "next/head";
 import Anchor from "@/components/Header/Anchor";
 import styles from "./Home.module.css";
+import { useState, useEffect } from "react";
 
 export default function Dashboard() {
+  const [sellers, setSellers] = useState("");
+
+  useEffect(() => {
+    fetch("/api/supabase-to-broker", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setSellers(data.response));
+  }, []);
+  console.log(`Sellers: `, sellers);
   return (
     <>
       <Head>
@@ -18,6 +32,15 @@ export default function Dashboard() {
             estate sellers below. Delete sellers or click on them to see more
             about them and their chosen buyers.
           </p>
+        </div>
+        <div className={styles.content}>
+          <h2>Choose a seller</h2>
+
+          {sellers.map((seller) => (
+            <li key={seller.id}>
+              <p>{seller.name}</p>
+            </li>
+          ))}
         </div>
       </div>
     </>
