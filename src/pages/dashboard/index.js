7772b56b9a79d423d36765622287539cacec7539
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2023-04-29 11:51:27 +0200

    Missing something

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -10,6 +10,30 @@ import { Avatar, Card, Skeleton, Switch } from "antd";
 export default function Dashboard() {
   const [sellers, setSellers] = useState("");
 
+  //GDPR DELETE
+  // Data from superbase returns like this:
+  // "2023-04-28T11:24:36.691055+00:00"
+  // So we'll need to match the date format with "YYYY-MM-DD" and have date and month be two numbers (01 insted of 1)
+
+  //Find todays date
+  const todayDate = getTodayDate();
+
+  function getTodayDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    // console.log("Year:", year);
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    // console.log("Month:", month);
+    const day = String(today.getDate()).padStart(2, "0");
+    // console.log("Day:", day);
+    const todayDate = year + "-" + month + "-" + day;
+    return todayDate;
+  }
+
+  //Delete those that matches dates
+
+  //GDPR DELETE
+
   useEffect(() => {
     fetch("/api/supabase-to-broker", {
       method: "GET",
@@ -18,9 +42,41 @@ export default function Dashboard() {
       },
     })
       .then((res) => res.json())
-      .then((data) => setSellers(data.response));
-  }, []);
-  // console.log(`Sellers: `, sellers);
+      .then((data) => {
+        //Check every sellers created_at date, if match --> Run function that deletes the entry
+        {
+          data.response.map((seller) => {
+            let sd = seller.created_at.substring(
+              0,
+              seller.created_at.indexOf("T")
+            );
+            if (sd === todayDate) {
+              deleteEntryGDPR(seller.id);
+              // console.log(seller.id);
+            }
+          });
+        }
+
+        setSellers(data.response);
+      });
+  }, [todayDate]);
+
+  function deleteEntryGDPR(id) {
+    fetch(
+      `https://pidnszjfygdazvvfuozh.supabase.co/rest/v1/edc_seller_helper?id=eq.${id}`,
+      {
+        method: "delete",
+        headers: {
+          "Content-Type": "application/json",
+          Prefer: "return=representation",
+          apikey: process.env.SUPABASE_KEY,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <>
       <Head>
